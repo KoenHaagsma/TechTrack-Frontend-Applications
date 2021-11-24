3793ee5d194b5cb1dc5599d9ad6acbc4c6be4284
Koen Haagsma <koenhaagsma123@hotmail.nl>
2021-11-24 13:13:02 +0100

    Initial react setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import BarChart from './components/BarChart';
+import './style.css';
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>Height/Weight comparison</h1>
+
+            <BarChart></BarChart>
+        </div>
+    );
+}
---
+++ src/components/BarChart.js
@@ -0,0 +1,7 @@
+import useD3 from '../hooks/useD3';
+import React from 'react';
+import * as d3 from 'd3';
+
+function BarChart() {}
+
+export default BarChart;
